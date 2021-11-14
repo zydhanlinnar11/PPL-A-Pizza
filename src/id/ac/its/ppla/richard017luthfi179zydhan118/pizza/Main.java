76ac69b5a0223ebcc5f4670e2cbc6a8cb1372f9e
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-11-14 23:13:00 +0700

    Init Pizza Project

---
+++ src/id/ac/its/ppla/richard017luthfi179zydhan118/pizza/Main.java
@@ -0,0 +1,7 @@
+package id.ac.its.ppla.richard017luthfi179zydhan118.pizza;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/id/ac/its/ppla/richard017luthfi179zydhan118/pizza/Pizza.java
@@ -0,0 +1,4 @@
+package id.ac.its.ppla.richard017luthfi179zydhan118.pizza;
+
+public class Pizza {
+}
