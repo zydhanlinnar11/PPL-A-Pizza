dd4219ccd3c37f8cb26a84a1ab052fa5ee8f5771
ALuthfiH <56031025+ALuthfiH@users.noreply.github.com>
2021-11-15 13:38:37 +0700

    Update Main.java

--- src/id/ac/its/ppla/richard017luthfi179zydhan118/pizza/Main.java
+++ src/id/ac/its/ppla/richard017luthfi179zydhan118/pizza/Main.java
@@ -2,6 +2,8 @@ package id.ac.its.ppla.richard017luthfi179zydhan118.pizza;
 
 import id.ac.its.ppla.richard017luthfi179zydhan118.pizza.crust.CheeseCrust;
 import id.ac.its.ppla.richard017luthfi179zydhan118.pizza.toppings.MozzarellaTopping;
+import id.ac.its.ppla.richard017luthfi179zydhan118.pizza.toppings.MushroomTopping;
+import id.ac.its.ppla.richard017luthfi179zydhan118.pizza.toppings.BeefTopping;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +11,15 @@ public class Main {
 
         System.out.println(pizza);
         System.out.println(pizza.pizzaPrice());
+        
+        Pizza pizza2 = new Pizza(new MushroomTopping(), new CheeseCrust());
+
+        System.out.println(pizza2);
+        System.out.println(pizza2.pizzaPrice());
+        
+        Pizza pizza3 = new Pizza(new BeefTopping(), new CheeseCrust());
+
+        System.out.println(pizza3);
+        System.out.println(pizza3.pizzaPrice());
     }
 }
