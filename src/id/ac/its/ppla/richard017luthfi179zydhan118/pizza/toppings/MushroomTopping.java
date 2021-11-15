c06630dd43eaa28adf354d6d31b6ae5f63dece3b
ALuthfiH <56031025+ALuthfiH@users.noreply.github.com>
2021-11-15 13:34:47 +0700

    Create MushroomTopping.java

---
+++ src/id/ac/its/ppla/richard017luthfi179zydhan118/pizza/toppings/MushroomTopping.java
@@ -0,0 +1,13 @@
+package id.ac.its.ppla.richard017luthfi179zydhan118.pizza.toppings;
+
+public class MushroomTopping implements PizzaTopping {
+    @Override
+    public int getPrice() {
+        return 4000;
+    }
+
+    @Override
+    public String getName() {
+        return "Mushroom";
+    }
+}
